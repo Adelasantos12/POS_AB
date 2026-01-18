8e056075fe5e560c82fe92c0159013b393dfa07f
Adelasantos12 <adela.santos12@gmail.com>
2026-01-18 14:58:19 +0100

    Add files via upload

--- adele_pos/urls.py
+++ adele_pos/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     # La ruta '/admin/' seguirá mostrando el panel de administración de Django.
     path('admin/', admin.site.urls),
-
+    
     # La ruta raíz ('/') ahora será gestionada por el archivo urls.py de nuestra app 'boutique'.
     # Esto hace que nuestra app sea la página principal del sitio.
     path('', include('boutique.urls')),
